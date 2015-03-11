c8cd8ee7b7eb8b44ba97e0adae1e8ef56e36eb63
MatheaR <mathea.renaudie@gmail.com>
2015-03-11 23:20:47 +0100

    mathea : debut de connexion utilisateur

--- QcmGenerator/src/main/java/com/view/Servlet.java
+++ QcmGenerator/src/main/java/com/view/Servlet.java
@@ -1,6 +1,8 @@
 package com.view;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
@@ -9,6 +11,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.model.Utilisateur;
+import com.service.metier.UtilisateurMetier;
 
 /**
  * Servlet implementation class Servlet
@@ -16,40 +22,92 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Servlet")
 public class Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private HttpSession session;
+	private String urlConnexion, urlCreationQcm, urlCreationQuestion, urlEleveAccueil;
+	private String urlProfAccueil, urlQuestions, urlStatsQcm, urlAccueil ;
+	private String identifiant, motdepasse;
+	private UtilisateurMetier utilisateur;
     
     public Servlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see Servlet#init(ServletConfig)
-	 */
-	public void init(ServletConfig config) throws ServletException {
-		//urlIndex = getInitParameter(urlIndex);
+	public void init() {
+		urlConnexion = getInitParameter("urlConnexion");
+		urlCreationQcm = getInitParameter("urlCreationQcm");
+		urlCreationQuestion = getInitParameter("urlCreationQuestion");
+		urlEleveAccueil = getInitParameter("urlEleveAccueil");
+		urlProfAccueil = getInitParameter("urlProfAccueil");
+		urlQuestions = getInitParameter("urlQuestions");
+		urlStatsQcm = getInitParameter("urlStatsQcm");
 	}
 
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher requestDispatcher; 
-		requestDispatcher = request.getRequestDispatcher("/WEB-INF/View/CreationQcm.jsp");
-		requestDispatcher.forward(request, response);
+		/*RequestDispatcher requestDispatcher; 
+		requestDispatcher = request.getRequestDispatcher("/WEB-INF/View/Connexion.jsp");
+		requestDispatcher.forward(request, response);*/
+
+		String action = request.getParameter("action");
+		RequestDispatcher requestDispatcher;
+		
+		if(action == null)
+		{
+			requestDispatcher = request.getRequestDispatcher(urlConnexion);
+			requestDispatcher.forward(request, response);
+		}
+		
+		switch(action)
+		{
+			case "connexion":
+				identifiant = request.getParameter("identifiant");
+				motdepasse = request.getParameter("mdp");
+				/*List<Utilisateur> users = utilisateur.GetAll();
+				
+				for (Utilisateur utilisateur : users) {
+					if(utilisateur.getIdentifiant().equals(identifiant))
+					{
+						if(utilisateur.getMdp().equals(motdepasse))
+						{*/
+							requestDispatcher = request.getRequestDispatcher(urlConnexion);
+							requestDispatcher.forward(request, response);
+						/*}
+						else
+						{
+							
+						}
+					}
+				}*/
+				break;
+			case "prof":
+				requestDispatcher = request.getRequestDispatcher(urlProfAccueil);
+				requestDispatcher.forward(request, response);
+				break;
+		}
 	}
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		 urlAccueil = "http://localhost:8080/QcmGenerator/?action=connexion";
+		 response.sendRedirect(urlAccueil);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 	}
+
+    private void validationIdentifiant( String identifiant ) throws Exception {
+        if ( identifiant != null) {
+            throw new Exception( "Merci de saisir un identifiant valide." );
+        }
+    }
+
+    private void validationMotDePasse( String motDePasse ) throws Exception {
+        if ( motDePasse != null ) {
+            if ( motDePasse.length() < 3 ) {
+                throw new Exception( "Le mot de passe doit contenir au moins 3 caractères." );
+            }
+        } else {
+            throw new Exception( "Merci de saisir votre mot de passe." );
+        }
+    }
 
 }
--- QcmGenerator/src/main/webapp/WEB-INF/index.jsp
+++ QcmGenerator/src/main/webapp/WEB-INF/index.jsp
@@ -2,4 +2,6 @@
     pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 
-<p>ta mereuh</p>
+<!-- <c:redirect url="/?action=connexion"></c:redirect> -->
+
+<jsp:forward page="/WEB-INF/View/Connexion.jsp" />
